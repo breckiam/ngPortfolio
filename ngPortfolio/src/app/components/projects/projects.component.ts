d734cf9f31eac3dc72585998a070ce46be427c24
breckiam <breckiam@gmail.com>
2022-06-25 14:44:17 -0600

    Porject background images added, stylining changes

--- ngPortfolio/src/app/components/projects/projects.component.ts
+++ ngPortfolio/src/app/components/projects/projects.component.ts
@@ -8,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
-  p1: Project = new Project('Food Trucks Anonomus', 'http://54.177.221.5:8080/FoodTruckTracker/#/home', 'https://github.com/julianchae/FinalProject', '');
-  p2: Project = new Project('Member Token Generator', 'http://54.177.221.5:8080/MemberToken/', 'https://github.com/julianchae/FinalProject', '');
-  p3: Project = new Project('3D Print Tracker', 'http://54.177.221.5:8080/PrintTrackerREST/', 'https://github.com/julianchae/FinalProject', '');
+  p1: Project = new Project('Food Trucks Anonomus', 'http://54.177.221.5:8080/FoodTruckTracker/#/home', 'https://github.com/julianchae/FinalProject', 'assets/Photos/truck.png');
+  p2: Project = new Project('Member Token Generator', 'http://54.177.221.5:8080/MemberToken/', 'https://github.com/julianchae/FinalProject', 'assets/Photos/mbrToken.png');
+  p3: Project = new Project('3D Print Tracker', 'http://54.177.221.5:8080/PrintTrackerREST/', 'https://github.com/julianchae/FinalProject', 'assets/Photos/gummy-printer.png');
 
   constructor() { }
 
