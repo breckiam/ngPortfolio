680552447977146ee2eec631a5a14ff06bc58420
breckiam <breckiam@gmail.com>
2022-07-02 15:42:43 -0600

    changed photos from .png to .webp

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -9,9 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
-  p1: Project = new Project('Food Trucks Anonymous', 'http://54.177.221.5:8080/FoodTruckTracker/#/home', 'https://github.com/julianchae/FinalProject', 'assets/Photos/truck.png');
-  p2: Project = new Project('Member Token Generator', 'http://54.177.221.5:8080/MemberToken/', 'https://github.com/acary/MidtermProject', 'assets/Photos/mbrToken.png');
-  p3: Project = new Project('3D Print Tracker', 'http://54.177.221.5:8080/PrintTrackerREST/', 'https://github.com/breckiam/EventTrackerProject', 'assets/Photos/gummy-printer.png');
+  p1: Project = new Project('Food Trucks Anonymous', 'http://54.177.221.5:8080/FoodTruckTracker/#/home', 'https://github.com/julianchae/FinalProject', 'assets/Photos/truck.webp');
+  p2: Project = new Project('Member Token Generator', 'http://54.177.221.5:8080/MemberToken/', 'https://github.com/acary/MidtermProject', 'assets/Photos/mbrToken.webp');
+  p3: Project = new Project('3D Print Tracker', 'http://54.177.221.5:8080/PrintTrackerREST/', 'https://github.com/breckiam/EventTrackerProject', 'assets/Photos/gummy-printer.webp');
 
   constructor() { }
 
